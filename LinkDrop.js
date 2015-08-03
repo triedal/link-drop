cded3e534ab51cb5654a284f13caaecdcfe94037
rolpe <ronlipkin14@gmail.com>
2015-08-03 14:13:14 -0400

    Added 10 second wait between sends
    also added message for wait

--- LinkDrop.js
+++ LinkDrop.js
@@ -1,4 +1,7 @@
+Users = new Mongo.Collection(null);
+
 if (Meteor.isServer) {
+
   Meteor.methods({
     sendEmail: function (to, from, subject, text) {
       check([to, from, subject, text], [String]);
@@ -18,37 +21,55 @@ if (Meteor.isServer) {
 }
 
 if (Meteor.isClient) {
-  Template.body.events({
-    "submit .email-sender-form": function(event) {
-      event.preventDefault();
 
-      var email = event.target.email.value;
-      var link = event.target.url.value;
+    Template.body.events({
+        "submit .email-sender-form": function (event) {
+            event.preventDefault();
 
-      if (email && link) {
-        Meteor.call('sendEmail',
-          email,
-          email,
-          'Link Drop',
-          link);
-        Session.set("sent", true); //used in messageSent helper to make it return true if email was sent.
-        event.target.email.value = ""; //makes input boxes empty after sending email
-        event.target.url.value = "";
-      }
-    },
-    "focus .emailTextBox": function(event) { //Both of these events will remove "status-message" when either text boxes
-      Session.set("sent", false);            //are clicked by making messageSent return false
-    },
-    "focus .urlTextBox": function(event) {
-      Session.set("sent", false);
-    }
-  });
+            var email = event.target.email.value;
+            var link = event.target.url.value;
+            var now = new Date();
+            if(Users.find().count() === 0) {
+                Users.insert({
+                    sentAt: now,
+                    canSend: now,
+                });
+            }
+            if (email && link && now >= Users.findOne().canSend) {
+                Meteor.call('sendEmail',
+                    email,
+                    email,
+                    'Link Drop',
+                    link);
+                Session.set("sent", true); //used in messageSent helper to make it return true if email was sent.
+                Session.set("wait", false);
+                event.target.email.value = ""; //makes input boxes empty after sending email
+                event.target.url.value = "";
+                Users.update(Users.findOne(), { $set: {sentAt: now, canSend: now.setSeconds(now.getSeconds() + 10)}})
+            } else if (email && link && now < Users.findOne().canSend) {
+                Session.set("wait", true);
+            }
+        },
+        "focus .emailTextBox": function (event) { //Both of these events will remove "status-message" when either text boxes
+            Session.set("sent", false);            //are clicked by making messageSent return false
+        },
+        "focus .urlTextBox": function (event) {
+            Session.set("sent", false);
+        }
+    });
 
-  Template.body.helpers({
-    messageSent: function() {
-      if(Session.get("sent")) {
-        return true;
-      }
-    }
-  })
+    Template.body.helpers({
+        messageSent: function () {
+            if (Session.get("sent")) {
+                return true;
+            }
+        },
+
+        cannotSendYet: function () {
+            if (Session.get("wait")) {
+                return true;
+            }
+        }
+    });
 }
+
